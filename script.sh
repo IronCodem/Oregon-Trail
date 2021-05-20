0a857957581583fef58f8d8d117c3fe76c0a74bf
Yashas Shah <79524299+coolcoder-js@users.noreply.github.com>
2021-05-20 17:50:38 -0400

    Refurbish Script

--- script.sh
+++ script.sh
@@ -5,18 +5,28 @@ sigSymbol_2=[      ]        [ ]
 sigSymbol_3=[      ]        [ ]
 sigSymbol_4=\______/    
 
+# Preface
 echo Heylo, who is dis?
 read name
 echo Oh, hai, $name! Get ready for another version of...OREGON TRAIL!
-echo $newLine
 
 echo $newLine
+echo Do you copy?
+read reply
+
+# Intro
 echo $newLine
 echo $newLine
 echo $newLine
+echo $newLine
+echo $newLine
+echo $newLine
+echo $newLine
+echo $newLine
+
 echo [][][][][][][][][][][][][][][][]
 echo [] ========================== []
-echo [] OREGON TRAIL-SHELL VERSION []
+echo [] OREGON TRAIL-.BASH EDITION []
 echo [] ========================== []
 echo [] coolcoder-js -- YashasShah []
 echo [] ========================== []
@@ -25,7 +35,7 @@ echo $newLine
 echo $newLine
 echo $newLine
 echo $newLine
-echo $newLine
-echo $newLine
-echo $newLine
-echo $newLine
+
+# Plot
+# > Choose job
+echo Pick a job:
